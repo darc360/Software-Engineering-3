1e4e078b512106da5c51e2912fd72ad4238c1d73
darc360 <56309328+darc360@users.noreply.github.com>
2020-11-20 20:48:23 +0000

    Update CourseProgramme.java
    Tostrong

--- src/main/java/CourseProgramme.java
+++ src/main/java/CourseProgramme.java
@@ -63,4 +63,8 @@ public class CourseProgramme {
     public void setEnd(DateTime end) {
         this.end = end;
     }
+    @Override
+    public String toString(){
+        return courseName;
+    }
 }
